adc42f30cef3e84b205557511b5513120d111107
Sulic Octavian <octavius1992@gmail.com>
2023-02-10 16:42:09 +0200

    Update css

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 function Score({ current, best }) {
   return (
     <div className="score">
+      <p>Memory Card game</p>
       <p>Top score: 18</p>
-      <p>Current score: {current}</p>
       <p>Best score: {best}</p>
+      <p>Current score: {current}</p>
     </div>
   );
 }
